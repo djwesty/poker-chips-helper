558a34b535c04de79b2b7f7302851bd4263982ea
vutukuri15 <vutukuri@pdx.edu>
2025-02-21 13:57:27 -0800

    Added configuration to support babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: [
+    "module:metro-react-native-babel-preset",
+    "babel-preset-expo",
+    "@babel/preset-typescript",
+  ],
   plugins: [
     [
       "module:react-native-dotenv",
