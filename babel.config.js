07c93e8800b11dc5c73f3b9c901a782cc9528af4
vutukuri15 <vutukuri@pdx.edu>
2025-02-23 16:32:38 -0800

    Fixed Welcome Screen Issue

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,6 @@
-module.exports = {
-  presets: ["babel-preset-expo", "@babel/preset-typescript"],
-  plugins: [
-    [
-      "module:react-native-dotenv",
-      {
-        moduleName: "@env",
-        path: ".env",
-        safe: true,
-        allowUndefined: false,
-      },
-    ],
-    "react-native-reanimated/plugin",
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+  };
 };
